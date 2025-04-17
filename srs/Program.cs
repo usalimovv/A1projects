a01b7eff73cb261a1d6fcd4d83c50d74a256e865
usalimovv <umidsalimov2004@gmail.com>
2025-04-17 21:13:53 +0500

    Ozgarddi

--- srs/Program.cs
+++ srs/Program.cs
@@ -4,9 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter number!");
+            var num = Convert.ToInt32(Console.ReadLine());
+
+            var ones = num % 10;
+            var tens = (num / 10) % 10;
+            var hunders = (num / 100) % 10;
+            var tsousands = (num / 1000) % 10;
+
+            var res = ones + tens + hunders + tsousands;
+
+            Console.Write("res : " + res );
+
+
         }
     }
 }
--- srs/obj/Debug/net8.0/srs.AssemblyInfo.cs
+++ srs/obj/Debug/net8.0/srs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("srs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b587a0a30d5d7103d0ffe7c7291d0aa7d69c21")]
 [assembly: System.Reflection.AssemblyProductAttribute("srs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("srs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
